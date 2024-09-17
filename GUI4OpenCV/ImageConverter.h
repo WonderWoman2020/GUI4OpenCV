1880980b1d903ccf15b45295f9d1233011ba4b73
WonderWoman <74731481+WonderWoman2020@users.noreply.github.com>
2024-09-17 02:35:57 +0200

    Added documentation for whole ImageConverter class

--- GUI4OpenCV/ImageConverter.h
+++ GUI4OpenCV/ImageConverter.h
@@ -10,7 +10,20 @@
 class ImageConverter
 {
 public:
+	/*
+	 * Converts image from Qt's format (QPixmap) to OpenCV format (cv::Mat). Converted image is of depth of 3 channels.
+	 * Converted image IS NOT a copy, it uses the same image data buffer as provided image.
+	 * @param image - image in Qt's QPixmap format.
+	 * @return Converted image in OpenCV cv::Mat format. 
+	 */
 	static cv::Mat convertQPixmapToMat(QPixmap& image);
+
+	/*
+	 * Converts image from OpenCV format (cv::Mat) to Qt's format (QPixmap). Converted image is of depth of 3 channels.
+	 * Converted image IS NOT a copy, it uses the same image data buffer as provided image.
+	 * @param image - image in OpenCV cv::Mat format.
+	 * @return Converted image in Qt's QPixmap format.
+	 */
 	static QPixmap convertMatToQPixmap(cv::Mat& image);
 
 };
