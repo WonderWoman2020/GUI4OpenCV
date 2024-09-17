d01914878df8de78b8eeda4f608b50fdbefa8d2a
WonderWoman <74731481+WonderWoman2020@users.noreply.github.com>
2024-09-17 03:14:58 +0200

    Added documentation for the whole AlphaSlider class

--- GUI4OpenCV/AlphaSlider.h
+++ GUI4OpenCV/AlphaSlider.h
@@ -19,11 +19,23 @@ public:
 	~AlphaSlider();
 
 public slots:
+	/**
+	 * Slot that is activated when slider value changes (when it was moved).
+	 * It updates a label that is over the slider with the current value.
+	 * @params value - current value of the slider, that will be set to its label.
+	 */
 	void changeSliderLabel(int value);
 
 signals:
+	/**
+	 * Signal that is send when slider value changes (when it was moved).
+	 * It should be received by some outer object, that wants to read the slider value.
+	 */
 	void sliderValueChanged(int value);
 
 private:
+	/**
+	 * View of the slider was created as a static XML .ui file, so the ui field is present for accessing the view UI elements.
+	 */
 	Ui::AlphaSliderClass *ui;
 };
