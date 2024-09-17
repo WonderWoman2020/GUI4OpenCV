66a5cda148186f7399aeca8f46435ce0fd8d60f3
WonderWoman <74731481+WonderWoman2020@users.noreply.github.com>
2024-09-17 02:50:03 +0200

    Added documentation for whole ImageLoader class

--- GUI4OpenCV/ImageLoader.h
+++ GUI4OpenCV/ImageLoader.h
@@ -16,9 +16,36 @@ public:
 	ImageLoader();
 	~ImageLoader();
 
+	/**
+	 * Reads image at the provided path. Allowed image file formats: JPG, JPEG, PNG and BMP.
+	 * @param path - path to image. Contains the filename in it as well.
+	 * @return Loaded image or empty image, if loading failed.
+	 */
 	cv::Mat readInImage(std::string path);
+
+	/*
+	 * Saves image at the provided path. Allowed image file formats: JPG, JPEG, PNG and BMP.
+	 * @param path - path for saving image. Contains the filename in it as well.
+	 * @param image - the image to be saved.
+	 * @return True - saving operation success, False - operation failed.
+	 */
 	bool saveImage(std::string path, cv::Mat& image);
 
+	/**
+	 * Handles 'open image' dialog. Shows adequate information windows throughout the operation.
+	 * Loads the image chosen by user to memory. Allowed image file formats: JPG, JPEG, PNG and BMP.
+	 * @param parent - a parent window for the 'open image' dialog window. All windows should have a parent, so Qt will take care of their deletion for us.
+	 * @return - Loaded image chosen by user, or
+	 * - empty image, if something failed while opening it.
+	 */
 	cv::Mat getImageDialog(QWidget* parent);
+
+	/**
+	 * Handles 'save image' dialog. Shows adequate information windows throughout the operation.
+	 * Saves the image in a file at path chosen by user. Allowed image file formats: JPG, JPEG, PNG and BMP.
+	 * @param parent - a parent window for the 'open image' dialog window. All windows should have a parent, so Qt will take care of their deletion for us.
+	 * @param image - the image to be saved.
+	 * @return True - saving operation success, False - operation failed.
+	 */
 	bool saveImageDialog(QWidget* parent, cv::Mat& image);
 };
